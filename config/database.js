9e9949b2f01810d664d8581f090f2344e35abe3d
Moe Mshawrab <Mchawrab-mhd>
2024-04-30 10:43:21 +0300

    dev udpate

--- config/database.js
+++ config/database.js
@@ -1,17 +1,14 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: 'postgres',
     connection: {
-      host: env("DATABASE_HOST", "strapi-properties-listing.c524k8guo6m9.me-central-1.rds.amazonaws.com"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "postgres"),
-      ssl: {
-          rejectUnauthorized: false, // Add this line to bypass certificate validation
-        },
-
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'postgres'),
+      ssl: false
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
